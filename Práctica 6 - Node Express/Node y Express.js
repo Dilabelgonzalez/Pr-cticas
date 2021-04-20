54d81200b4aceafecf0fcaa3267ae5e614a03311
Dilabel González <75492469+Dilabelgonzalez@users.noreply.github.com>
2021-04-20 19:23:05 -0400

    Delete Node y Express.js

--- Práctica 6 - Node Express/Node y Express.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/Inicio', (req, res) => res.send('Ciao a tutti!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
